dfdcab9774ced1eee495cfe33f289ee68da666e3
Kiripupsik5 <kirillpup227@gmail.com>
2023-06-23 19:36:49 +0300

    add lose window

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ WIN_WIDTH = 600
 WIN_HEIGHT = 600
 FPS = 60
 
+pygame.mixer_music.load(file_path(r"musics_labirint\fon1.mp3"))
+pygame.mixer_music.set_volume(0.25)
+pygame.mixer_music.play(-1)
+
 background = pygame.image.load(file_path(r"images_labirint\black fon.jpg"))
 background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
+
+victory_image = pygame.image.load(r"images_labirint\WINNER FON.png")
+victory_image = pygame.transform.scale(victory_image, (WIN_WIDTH, WIN_HEIGHT))
+
+lose_image = pygame.image.load(r"images_labirint\LOSER FON.png")
+lose_image = pygame.transform.scale(lose_image, (WIN_WIDTH, WIN_HEIGHT))
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
@@ -39,13 +49,36 @@ class Player(GameSprite):
     def update(self):
         if self.speed_x < 0 and self.rect.left > 0 or self.speed_x > 0 and self.rect.right < WIN_WIDTH:
             self.rect.x += self.speed_x
+        walls_touched = pygame.sprite.spritecollide(self, walls, False)
+        if self.speed_x > 0:
+            for wall in walls_touched:
+                self.rect.right = min(self.rect.right, wall.rect.left)
+        if self.speed_x < 0:
+            for wall in walls_touched:
+                self.rect.left = max(self.rect.left, wall.rect.right)
+
         if self.speed_y < 0 and self.rect.top > 0 or self.speed_y > 0 and self.rect.bottom < WIN_HEIGHT:
             self.rect.y += self.speed_y
+        
+        walls_touched = pygame.sprite. spritecollide(self, walls, False)
+        if self.speed_y < 0:
+            for wall in walls_touched:
+                self.rect.top = max(self.rect.top, wall.rect.bottom)
+        if self.speed_y > 0:
+            for wall in walls_touched:
+                self.rect.bottom = min(self.rect.bottom, self.rect.top)
 
 
 player = Player(15, 15, 15, 15, r"images_labirint\player.png", 0, 0)
-enemy1 = GameSprite(155, 155, 15, 15, r"images_labirint\enemy.png")
 finish = GameSprite(555, 555, 15, 15, r"images_labirint\finish.png")
+
+enemies = pygame.sprite.Group()
+
+enemy1 = GameSprite(170, 155, 15, 15, r"images_labirint\enemy.png")
+enemy2 = GameSprite(275, 155, 15, 15, r"images_labirint\enemy.png")
+enemies.add(enemy1)
+enemies.add(enemy2)
+
 
 #название стен_расположение стен v-vertical,  h-horizontal_номер рядка в котором расположенны стены_номер редактируемой стены
 walls = pygame.sprite.Group()
@@ -430,12 +463,31 @@ while game:
         walls.draw(window)
         player.show()
         player.update()     
-        enemy1.show()
+        enemies.draw(window)
         finish.show()
+
+        if pygame.sprite.collide_rect(player, finish):
+            level = 10
+            pygame.mixer_music.stop()
+            pygame.mixer_music.load(file_path(r"musics_labirint\win1.mp3"))
+            pygame.mixer_music.set_volume(0.10)
+            pygame.mixer_music.play(-1)
+    
+        if pygame.sprite.spritecollide(player, enemies, False):
+            level = 11
+            pygame.mixer_music.stop()
+            pygame.mixer_music.load(file_path(r"musics_labirint\lose1.mp3"))
+            pygame.mixer_music.set_volume(0.10)
+            pygame.mixer_music.play(1)
+
+    elif level == 10:
+        window.blit(victory_image, (0, 0))
+
+    elif level == 11:
+        window.blit(lose_image, (0, 0))
 
     clock.tick(FPS)
     pygame.display.update()
-
 
 
 
